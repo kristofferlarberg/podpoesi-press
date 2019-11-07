c4887989d9e2e086729e3abbf9fe6561d8fd386d
Kristoffer Larberg <mail@kristofferlarberg.se>
2019-11-07 10:23:33 +0100

    Validering och Audit OK

--- script.js
+++ script.js
@@ -3,35 +3,13 @@ var myForm = document.forms.myForm;
 var message = document.getElementById("message");
 
 myForm.onsubmit = function() {
+  
   if(myForm.name.value == ""){
     message.innerHTML = "Skriv in en giltig e-postadress";
     return false;
   } else { 
-    message.innerHTML = "";
-    return true;
+    message.innerHTML = "Tack för ditt intresse";
+    myForm.name.value = ""
+    return false;
   }
 };
-
-//Klocka
-/* function currentTime() {
-  let date = new Date(); //creating object of Date class
-  let hour = date.getHours();
-  let min = date.getMinutes();
-  let sec = date.getSeconds();
-  hour = updateTime(hour);
-  min = updateTime(min);
-  sec = updateTime(sec);
-  document.getElementById("clock").innerText = hour + ":" + min + ":" + sec; //adding time to the div
-  let t = setTimeout(function () { currentTime() }, 1000); //setting timer
-}
-
-function updateTime(k) {
-  if (k < 10) {
-    return "0" + k;
-  }
-  else {
-    return k;
-  }
-}
-
-currentTime(); //calling currentTime() function to initiate the process */
